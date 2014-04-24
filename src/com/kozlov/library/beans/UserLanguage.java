515e98083a3703166b0872d39f2e2b6825561f1d
dima <dima_16_90@ukr.net>
2014-04-24 19:29:30 +0300

    finished with lang

--- src/com/kozlov/library/beans/UserLanguage.java
+++ src/com/kozlov/library/beans/UserLanguage.java
@@ -12,43 +12,31 @@ import javax.faces.event.ValueChangeEvent;
 
 
 
-@ManagedBean(eager=true)
+@ManagedBean(eager = true)
 @SessionScoped
 public class UserLanguage implements Serializable {
 
-   private static final long serialVersionUID = 1L;
-   private String locale;
+    private static final long serialVersionUID = 1L;
+    private Locale locale = FacesContext.getCurrentInstance().getApplication()
+            .getDefaultLocale();
 
-   private static Map<String,Object> countries;
-   
-   static{
-      countries = new LinkedHashMap<String,Object>();
-      countries.put("English", new Locale("en"));
-      countries.put("Russian", new Locale("ru"));
-      countries.put("Ukrainian", new Locale("uk"));
-   }
+    public String[] getCountries() {
 
-   public Map<String, Object> getCountries() {
-      return countries;
-   }
+        return new String[] { "en", "ru", "uk" };
+    }
 
-   public String getLocale() {
-      return locale;
-   }
+    public Locale getLocale() {
+        return locale;
+    }
 
-   public void setLocale(String locale) {
-      this.locale = locale;
-   }
+    public void setLanguage(String lang) {
+        locale = new Locale(lang);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
 
-   //value change event listener
-   public void localeChanged(ValueChangeEvent e){
-      String newLocaleValue = e.getNewValue().toString();
-      for (Map.Entry<String, Object> entry : countries.entrySet()) {
-         if(entry.getValue().toString().equals(newLocaleValue)){
-            FacesContext.getCurrentInstance()
-               .getViewRoot().setLocale((Locale)entry.getValue());         
-         }
-      }
-   }
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
 }
 
